5bb5ddc22abd50af3bfb466a311cec407fae5533
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-07-28 02:06:41 -0300

    Exception handling - findById

--- studyspring/src/main/java/com/lonftonfando/studyspring/service/UserService.java
+++ studyspring/src/main/java/com/lonftonfando/studyspring/service/UserService.java
@@ -3,6 +3,7 @@ package com.lonftonfando.studyspring.service;
 import com.lonftonfando.studyspring.entities.User;
 import com.lonftonfando.studyspring.repositories.UserRepository;
 
+import com.lonftonfando.studyspring.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
